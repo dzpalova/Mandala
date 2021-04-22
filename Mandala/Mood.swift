2c7977928413b72bdd3308631b5d3742a54923de
Daniela Palova <daniela.palova@infinno.eu>
2021-04-22 13:07:26 +0300

    Initial commit

---
+++ Mandala/Mood.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+struct Mood {
+    var name: String
+    var image: UIImage
+    var color: UIColor
+    
+    init(_ resource: ImageResource) {
+        self.name = resource.rawValue
+        self.image = UIImage(resource: resource)
+        self.color = UIColor.colors[resource]!
+    }
+}
+
+struct MoodEntry {
+    var mood: Mood
+    var timestamp: Date
+}
+
+extension Mood {
+    static let angry = Mood(.angry)
+    static let confused = Mood(.confused)
+    static let crying = Mood(.crying)
+    static let goofy = Mood(.goofy)
+    static let happy = Mood(.happy)
+    static let meh = Mood(.meh)
+    static let sad = Mood(.sad)
+    static let sleepy = Mood(.sleepy)
+}
